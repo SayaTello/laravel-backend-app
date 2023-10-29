911a05e41987d0bee609b849aefb34b46462c7cd
TELLO <201240001068@unisnu.ac.id>
2023-10-29 23:18:30 +0700

    crud dan authorization with policy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([UserSeeder::class]);
+        Category::factory(5)->create();
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class
+        ]);
     }
 }
